89ae91a19adfc4bfdbcc883245997e663604abe9
cc <codecolorist@gmail.com>
2026-02-13 02:01:59 +0100

    refactor arguments dump

--- agent/src/nsxpc.ts
+++ agent/src/nsxpc.ts
@@ -5,7 +5,23 @@ import { find, glob } from "./symbol.js";
 ObjC.classes.NSBundle.bundleWithPath_(
   "/System/Library/Frameworks/Foundation.framework",
 ).load();
+
 const { NSXPCListener, NSXPCInterface, NSXPCConnection } = ObjC.classes;
+
+function formatArgs(
+  args: InvocationArguments,
+  signature: ObjC.Object,
+): string[] {
+  const nargs = signature.numberOfArguments();
+  const result: string[] = [];
+  for (let i = 2; i < nargs; i++) {
+    const arg = args[i];
+    const t = signature.getArgumentTypeAtIndex_(i);
+    const wrapped = t.toString().startsWith("@") ? new ObjC.Object(arg) : arg;
+    result.push(wrapped.toString());
+  }
+  return result;
+}
 
 export function listeners() {
   return ObjC.chooseSync(NSXPCListener).map((listener) => {
@@ -36,22 +52,12 @@ export function start() {
 
       this.hook = Interceptor.attach(imp, {
         onEnter(innerArgs) {
-          const nargs = signature.numberOfArguments();
-          const formattedArgs: string[] = [];
-          for (let i = 2; i < nargs; i++) {
-            // skip self and selector
-            const arg = innerArgs[i];
-            const t = signature.getArgumentTypeAtIndex_(i);
-            const wrapped = t.toString().startsWith("@")
-              ? new ObjC.Object(arg)
-              : arg;
-            formattedArgs.push(wrapped.toString());
-          }
+          const beautifiedArgs = formatArgs(innerArgs, signature);
 
           const json = {
             type: "nsxpc",
             sel,
-            args: formattedArgs,
+            args: beautifiedArgs,
             description: sel,
           };
 
@@ -81,17 +87,7 @@ export function start() {
         const targetClass = new ObjC.Object(args[0]);
         const sel = ObjC.selectorAsString(args[1]);
         const signature = targetClass.methodSignatureForSelector_(args[1]);
-        const nargs = signature.numberOfArguments();
-        const formattedArgs: string[] = [];
-        for (let i = 2; i < nargs; i++) {
-          // skip self and selector
-          const arg = args[i];
-          const t = signature.getArgumentTypeAtIndex_(i);
-          const wrapped = t.toString().startsWith("@")
-            ? new ObjC.Object(arg)
-            : arg;
-          formattedArgs.push(wrapped.toString());
-        }
+        const formattedArgs = formatArgs(args, signature);
 
         const json = {
           type: "nsxpc",
@@ -130,22 +126,13 @@ export function start() {
     Interceptor.attach(func, {
       onEnter(args) {
         const sel = ObjC.selectorAsString(args[1]);
-        const formattedArgs: string[] = [];
         const proxy = new ObjC.Object(args[0]);
         const clazz = proxy.$className;
 
         const signature = proxy.methodSignatureForSelector_(
           args[1],
         ) as ObjC.Object;
-        const nargs = signature.numberOfArguments();
-        for (let i = 2; i < nargs; i++) {
-          const arg = args[i];
-          const t = signature.getArgumentTypeAtIndex_(i);
-          const wrapped = t.toString().startsWith("@")
-            ? new ObjC.Object(arg)
-            : arg;
-          formattedArgs.push(wrapped.toString());
-        }
+        const beautifiedArgs = formatArgs(args, signature);
 
         const conn = new ObjC.Object(
           ObjC.getBoundData(args[0]).conn as NativePointer,
@@ -156,7 +143,7 @@ export function start() {
         const json = {
           type: "nsxpc",
           sel,
-          args: formattedArgs,
+          args: beautifiedArgs,
           description: `${clazz} ${sel}`,
         };
 
